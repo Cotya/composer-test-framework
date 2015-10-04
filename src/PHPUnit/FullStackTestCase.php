6be0a170c79a93e8a6a28c0f4ce3bc139f135aa2
Aydin Hassan <aydin@hotmail.co.uk>
2015-10-04 18:40:29 +0100

    Shorter directory on Windows
    Windows temporary directory is very deep and causes some tests to fail

--- src/PHPUnit/FullStackTestCase.php
+++ src/PHPUnit/FullStackTestCase.php
@@ -27,6 +27,11 @@ abstract class FullStackTestCase extends \PHPUnit_Framework_TestCase
     
     protected static function getTempComposerProjectPath()
     {
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+            $path = sprintf("C:/ComposerTests/%s", str_replace('\\', '_', get_called_class()));
+            return new \SplFileInfo($path);
+        }
+
         $path = sprintf(
             "%s/ComposerTests/%s",
             str_replace('\\', '/', sys_get_temp_dir()),
